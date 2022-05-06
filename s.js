a38cd53257d59039c588b57d3c0c15662f1ea86b
Azzkaarr <97603230+Azzkaarr@users.noreply.github.com>
2022-05-06 00:39:52 +0000

    Add files via upload

---
+++ s.js
@@ -0,0 +1,40 @@
+setTimeout(function(){
+document.getElementById("tim1").innerHTML = "<h4>beef burger 30$</h4>";
+},1000);
+setTimeout(function(){
+  document.getElementById("tim2").innerHTML = "<h4>chicken burger 40$</h4>";
+  },2000);
+  setTimeout(function(){
+    document.getElementById("tim3").innerHTML = "<h4>tacos mexico 80$</h4>";
+    },3000);
+    setTimeout(function(){
+      document.getElementById("pp").innerHTML = "<img src='image/keba.png' width='500px'>";
+      },4000);
+      setTimeout(function(){
+        document.getElementById("ll").innerHTML = "<img src='image/promo2.png' width='150px'>";
+        },5000);
+        setTimeout(function(){
+          document.getElementById("QQ").innerHTML = "<img src='image/keba.png' width='400px'>";
+          },4000);
+          setTimeout(function(){
+            document.getElementById("xr").innerHTML = "<img src='image/keba.png' width='230px'>";
+            },4000);
+            setTimeout(function(){
+              document.getElementById("PROS").innerHTML = "<img src='image/promo2.png' width='100px'>";
+              },5000);
+              setTimeout(function(){
+                document.getElementById("XR").innerHTML = "<img src='image/keba.png' width='350px'>";
+                },4000);
+                setTimeout(function(){
+                  document.getElementById("XR1").innerHTML = "<img src='image/keba.png' width='300px'>";
+                  },4000);
+                  setTimeout(function(){
+                    document.getElementById("xr1").innerHTML = "<img src='image/keba.png' width='200px'>";
+                    },3000);
+                    setTimeout(function(){
+                      document.getElementById("PROS1").innerHTML = "<img src='image/promo2.png' width='80px'>";
+                      },4000);
+
+  
+
+
